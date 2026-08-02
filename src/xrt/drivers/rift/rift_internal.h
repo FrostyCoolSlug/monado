ef39340ebd3c5ed9b05d61179295bc2b3c772a00
Beyley Cardellio <ep1cm1n10n123@gmail.com>
2026-08-01 23:50:31 -0700

    d/rift: Port to new distortion implementation
    Signed-off-by: Beyley Cardellio <ep1cm1n10n123@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2961>

--- src/xrt/drivers/rift/rift_internal.h
+++ src/xrt/drivers/rift/rift_internal.h
@@ -14,6 +14,7 @@
 
 #include "util/u_device.h"
 #include "util/u_logging.h"
+#include "util/u_distortion_mesh.h"
 
 #include "math/m_imu_3dof.h"
 #include "math/m_api.h"
@@ -207,7 +208,6 @@ struct rift_display_info_report
 SIZE_ASSERT(struct rift_display_info_report, 55);
 
 #define CATMULL_COEFFICIENTS 11
-#define CHROMATIC_ABBERATION_COEFFEICENT_COUNT 4
 
 struct rift_catmull_rom_distortion_report_data
 {
@@ -217,7 +217,7 @@ struct rift_catmull_rom_distortion_report_data
 	uint16_t k[CATMULL_COEFFICIENTS];
 	uint16_t max_r;
 	uint16_t meters_per_tan_angle_at_center;
-	uint16_t chromatic_abberation[CHROMATIC_ABBERATION_COEFFEICENT_COUNT];
+	uint16_t chromatic_abberation[U_RIFT_CHROMATIC_ABBERATION_COUNT];
 	uint8_t unused[14];
 };
 
@@ -629,7 +629,7 @@ struct rift_catmull_rom_distortion_data
 	float k[CATMULL_COEFFICIENTS];
 	float max_r;
 	float meters_per_tan_angle_at_center;
-	float chromatic_abberation[CHROMATIC_ABBERATION_COEFFEICENT_COUNT];
+	float chromatic_abberation[U_RIFT_CHROMATIC_ABBERATION_COUNT];
 };
 
 struct rift_lens_distortion
@@ -642,36 +642,6 @@ struct rift_lens_distortion
 	union {
 		struct rift_catmull_rom_distortion_data lcsv_catmull_rom_10;
 	} data;
-};
-
-struct rift_scale_and_offset
-{
-	struct xrt_vec2 scale;
-	struct xrt_vec2 offset;
-};
-
-struct rift_viewport_fov_tan
-{
-	float up_tan;
-	float down_tan;
-	float left_tan;
-	float right_tan;
-};
-
-struct rift_extra_display_info
-{
-	// gap left between the two eyes
-	float screen_gap_meters;
-	// the diameter of the lenses, may need to be extended to an array
-	float lens_diameter_meters;
-	// ipd of the headset
-	float icd;
-
-	// the fov of the headset
-	struct rift_viewport_fov_tan fov;
-	// mapping from tan-angle space to target NDC space
-	struct rift_scale_and_offset eye_to_source_ndc;
-	struct rift_scale_and_offset eye_to_source_uv;
 };
 
 struct rift_imu_calibration
@@ -971,7 +941,11 @@ struct rift_hmd
 	uint16_t num_lens_distortions;
 	uint16_t distortion_in_use;
 
-	struct rift_extra_display_info extra_display_info;
+	struct u_rift_panel panel;
+	struct u_rift_eye_profile eye_profile;
+
+	//! Interpupillary distance of the headset, in meters.
+	float default_icd;
 	float icd_override_m;
 
 	bool presence;
